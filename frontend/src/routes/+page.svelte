7b640403cc8c17e9de13c2776ea5e5dcf6893208
sturtevm-tamu <sturtevm@tamu.edu>
2024-12-15 04:54:18 -0600

    Better Formatting

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,28 +1,139 @@
 <script lang="ts">
     import {cn} from "$lib/utils";
 
-    const puzzle: ('S' | 'U' | 'M' | 'X')[][] = [
-        ['S', 'U', 'M'],
-        ['U', 'S', 'U'],
-        ['S', 'X', 'S']
+    const rawPuzzle: string[] = [
+        'XXXXXXXXXXXXXXX',
+        'XXXXXXXXXXXXXXX',
+        'XSXXXXXXXXXXXXX',
+        'SSSSXXXXXXXXXXX',
+        'SXSSSSXXXXXXXXX',
+        'SXSSXSSSSXXXXXX',
+        'SXSSSSSXSSSSSSX',
+        'SXSSSXSSSSXSXSS',
+        'XSXSSSSSXSSSXSS',
+        'XSSXSSSSSSSXSSS',
+        'XXSSXSSSSSXSSSX',
+        'XXXSSXXXXXSSSXX',
+        'XXXXSSSSSSSXXXX',
+        'XXXXXXXXXXXXXXX',
+        'XXXXXXXXXXXXXXX'
     ];
 
-    const sqSize: number = 20
+    function convertRawToPuzzle(raw: string[]) {
+        return raw.map((row) => row.split('') as ('S' | 'U' | 'M' | 'X')[]);
+    }
+
+    const puzzle: ('S' | 'U' | 'M' | 'X')[][] = $state(convertRawToPuzzle(rawPuzzle));
+
+    const rows: number[][] = $state([
+        [0],
+        [0],
+        [1],
+        [4],
+        [1, 4],
+        [1, 2, 4],
+        [1, 5, 6],
+        [1, 3, 4, 1, 2],
+        [1, 5, 3, 2],
+        [2, 7, 3],
+        [2, 5, 3],
+        [2, 3],
+        [7],
+        [0],
+        [0]
+    ]);
+
+    const cols: number[][] = $state([
+       [5],
+       [2, 2],
+       [5, 2],
+       [6, 2],
+       [1, 4, 2],
+       [3, 3, 1],
+       [6, 1],
+       [1, 4, 1],
+       [3, 2, 1],
+       [5, 1],
+       [1, 2, 2],
+       [3, 2],
+       [1, 3],
+       [5],
+       [3]
+    ]);
+
+    function flip(i: number, j: number, typ: string): void {
+        if (puzzle[i][j] === 'S') {
+            if (typ === "S")
+                puzzle[i][j] = 'U';
+        }
+        else if (puzzle[i][j] === 'U') {
+            if (typ === "S")
+                puzzle[i][j] = 'S';
+            else if (typ === "M")
+                puzzle[i][j] = 'M';
+        }
+        else if (puzzle[i][j] === 'M') {
+            if (typ === "S")
+                puzzle[i][j] = 'S';
+            else if (typ === "M")
+                puzzle[i][j] = 'U';
+        }
+
+    }
+
+    const TILE_SIZE: number = $state(40)
+    const MINI_SIZE: number = $derived(TILE_SIZE * 0.8)
+
 </script>
 
+<!--
+<button onclick={() => puzzle[0][0] = 'U'}>
+    hi
+</button>
+-->
+
 <div class="flex h-screen w-screen justify-center items-center">
-    <div class="grid grid-cols-2 grid-rows-2 place-items-center">
+    <div class="grid grid-cols-2 grid-rows-2">
         <div></div>
-        <div></div>
-        <div></div>
-        <div class="grid grid-cols-{puzzle.length} grid-rows-{puzzle.length} border-white border">
+
+        <div class="flex flex-row" style:padding-left="1px" style:padding-right="1px">
+            {#each cols as col, i}
+                <div class={cn("w-10 flex pb-2 items-center justify-end flex-col bg-gradient-to-b from-transparent border-transparent", (i % 2 === 0) ? "to-black/20" : "to-white/20",
+                            i !== 0 && "border-l-[0.5px]", i !== cols.length-1 && "border-r-[0.5px]")} style:gap="12px">
+                    {#each col as num}
+                        <div>
+                            <span class="text-black font-bold font-sans drop-shadow-[0_1.5px_1.5px_rgba(255,255,255,0.8)]" style:font-size="24px" style:line-height="24px">{num}</span>
+                        </div>
+                    {/each}
+                </div>
+            {/each}
+        </div>
+
+        <div class="flex flex-col" style:padding-top="1px" style:padding-bottom="1px">
+            {#each rows as row, i}
+                <div class={cn("h-10 flex pr-2 items-center justify-end flex-row bg-gradient-to-r from-transparent border-transparent", (i % 2 === 0) ? "to-black/20" : "to-white/20",
+                            i !== 0 && "border-t-[0.5px]", i !== rows.length-1 && "border-b-[0.5px]")} style:gap="24px">
+                    {#each row as num}
+                        <div>
+                            <span class="text-black font-bold font-sans drop-shadow-[0_1.5px_1.5px_rgba(255,255,255,0.8)]" style:font-size="24px" style:line-height="24px">{num}</span>
+                        </div>
+                    {/each}
+                </div>
+            {/each}
+        </div>
+
+
+        <div class="grid border-white border" style:grid-template-rows="repeat({puzzle.length}, minmax(0, 1fr))" style:grid-template-columns="repeat({puzzle[0].length}, minmax(0, 1fr))">
             {#each puzzle as row, i}
                 {#each row as cell, j}
-                    <div class={cn("size-10 bg-purple-400  place-content-center place-items-center aspect-square", "border-purple-200",
-                                i !== 0 && "border-t-[0.5px]", j !== 0 && "border-l-[0.5px]",
-                                i !== puzzle.length-1 && "border-b-[0.5px]", j !== puzzle.length-1 && "border-r-[0.5px]")}>
+                    <div class={cn("size-10 bg-purple-400 outline-none place-content-center place-items-center aspect-square", "border-purple-200 hover:border-amber-400",
+                                i !== 0 && "border-t-[0.5px]",
+                                j !== 0 && "border-l-[0.5px]",
+                                i !== puzzle.length-1 && "border-b-[0.5px]",
+                                j !== puzzle.length-1 && "border-r-[0.5px]")}
+                                onclick={() => flip(i, j, 'S')} role="button" tabindex="0" onkeyup={() => flip(i, j, 'M')}>
                         {#if cell === 'S'}
-                            <div class="size-8 rounded bg-white aspect-square"></div>
+                            <div class="rounded bg-white aspect-square" style:width="32px"></div>
                         {:else if cell === 'U'}
                             <div></div>
                         {:else if cell === 'M'}
@@ -49,4 +160,3 @@
 
 </div>
 
-<!-- grid-rows-3 grid-cols-3 -->
