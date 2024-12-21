668c8682922ecee669c26aba3ba07f47760ac415
Mark Sturtevant <sturtevm@gmail.com>
2024-12-21 02:54:47 -0600

    Fun Animation!

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -12,6 +12,58 @@
 	const cols: number[][] = $state([[1]]);
 	let puzzleSolution: string = '1';
 	let puzzleColor = $state("#c084fc");
+	let puzzleFinished = $state(false);
+
+	let remRows = 141241424, remCols = 1123234125;
+	function checkSolution(i: number, j: number): void {
+		const N = rows.length, M = cols.length;
+
+		// check the rows
+		const arr2: number[] = [];
+		let l = 0;
+		for (let k = 0; k < M; ++k) {
+			if (puzzle[i][k] === 'S')
+				l++;
+			else if (l > 0) {
+				arr2.push(l);
+				l = 0;
+			}
+		}
+		if (l > 0 || arr2.length === 0)
+			arr2.push(l);
+
+		// check the cols
+		const arr3: number[] = [];
+		l = 0;
+		for (let k = 0; k < N; ++k) {
+			if (puzzle[k][j] === 'S')
+				l++;
+			else if (l > 0) {
+				arr3.push(l);
+				l = 0;
+			}
+		}
+		if (l > 0 || arr3.length === 0)
+			arr3.push(l);
+
+		// update the remain variables
+		if (rows[i].join(',') === arr2.join(','))
+			remRows &= (1 << N) - 1 - (1 << i);
+		else
+			remRows |= (1 << i);
+
+		if (cols[j].join(',') === arr3.join(','))
+			remCols &= (1 << M) - 1 - (1 << j);
+		else
+			remCols |= (1 << j);
+
+
+		if (remRows === 0 && remCols === 0) {
+			// @ts-ignore solution is found!
+			document.getElementById('puzzle-reveal').style.display = "block";
+			puzzleFinished = true;
+		}
+	}
 
 	let userURL: string = 'ad1bjp5ogg316z8';
 	let gameURL: string = '31882z3yp737sjs';
@@ -29,17 +81,15 @@
 			document.body.style.backgroundColor = data.style_meta['background-color'];
 			puzzleColor = data.style_meta['puzzle-color'];
 
-			// set the background image
-			console.log(data.background_image);
-
-			// @ts-ignore
+			// @ts-ignore set the background image
 			document.getElementById('body-bg-image').style.backgroundImage = `url(${pb.files.getURL(data, data.background_image)})`;
 
-			/*
-			document.body.style.backgroundImage = `url(${pb.files.getURL(data, data.background_image)})`;
-			document.body.style.backgroundRepeat = 'no-repeat';
-			document.body.style.backgroundSize = 'cover';
-			document.body.style.background*/
+			// @ts-ignore set the puzzle image
+			document.getElementById('puzzle-reveal').style.backgroundImage = `url(${pb.files.getURL(data, data.puzzle_image)})`;
+
+			// set the correctRows / Cols variables
+			remRows = (1 << data.puzzle.rl) - 1;
+			remCols = (1 << data.puzzle.cl) - 1;
 
 			// generate an empty puzzle
 			puzzle.pop();
@@ -58,7 +108,10 @@
 				sort: 'created'
 			});
 			eventsPromise.then((eventsList) => {
-				for (let event of eventsList) puzzle[event.action.r][event.action.c] = event.action.t;
+				for (let event of eventsList) {
+					puzzle[event.action.r][event.action.c] = event.action.t;
+					checkSolution(event.action.r, event.action.c);
+				}
 			});
 
 			// parse the rows
@@ -74,7 +127,10 @@
 						curCnt = 0;
 					}
 				}
-				if (temp.length === 0) temp.push(0);
+				if (temp.length === 0) {
+					temp.push(0);
+					remRows -= (1 << i);
+				}
 				rows.push(temp);
 			}
 			if (rows.length === 0) rows.push([0]);
@@ -92,7 +148,10 @@
 						curCnt = 0;
 					}
 				}
-				if (temp.length === 0) temp.push(0);
+				if (temp.length === 0) {
+					temp.push(0);
+					remCols -= (1 << i);
+				}
 				cols.push(temp);
 			}
 		});
@@ -155,6 +214,8 @@
 			}
 		};
 		gameEventCollection.create(data);
+
+		checkSolution(i, j);
 	}
 
 	const clientID = nanoid();
@@ -314,55 +375,60 @@
 			{/each}
 		</div>
 
-		<div
-			class="grid border border-white"
-			style:grid-template-rows="repeat({puzzle.length}, minmax(0, 1fr))"
-			style:grid-template-columns="repeat({puzzle[0].length}, minmax(0, 1fr))"
-		>
-			{#each puzzle as row, i}
-				{#each row as cell, j}
-					<div
-						class={cn(
-							'aspect-square size-10 place-content-center place-items-center outline-none',
-							'border-white hover:border-amber-400 hover:border-2',
-							i !== 0 && 'border-t-[0.5px]',
-							j !== 0 && 'border-l-[0.5px]',
-							i !== puzzle.length - 1 && 'border-b-[0.5px]',
-							j !== puzzle.length - 1 && 'border-r-[0.5px]'
-						)}
-						onmouseenter={() => test_trigger(i, j)}
-						onmousedown={(e) => ev_mouse_down(i, j, e)}
-						onmouseup={() => ev_mouse_up()}
-						role="button"
-						tabindex="0"
-						style:background-color={puzzleColor}
-					>
-						{#if cell === 'S'}
-							<div transition:scale class="aspect-square rounded bg-white" style:width="32px"></div>
-						{:else if cell === 'U'}
-							<div transition:scale></div>
-						{:else if cell === 'M'}
-							<div
-									transition:scale
-									class="aspect-square size-8 rounded border-2 border-white"
-							></div>
-						{:else if cell === 'X'}
-							<div transition:scale class="relative aspect-square size-10">
-								<div class="absolute aspect-square size-10">
-									<div class="flex h-full w-full items-center justify-center">
-										<div class="h-8 w-0.5 rotate-45 rounded-full bg-black"></div>
+		<div class="relative">
+			<div
+				class="grid border border-white absolute"
+				style:grid-template-rows="repeat({puzzle.length}, minmax(0, 1fr))"
+				style:grid-template-columns="repeat({puzzle[0].length}, minmax(0, 1fr))"
+			>
+				{#each puzzle as row, i}
+					{#each row as cell, j}
+						<div
+							class={cn(
+								'aspect-square size-10 place-content-center place-items-center outline-none',
+								'border-white hover:border-amber-400 hover:border-2',
+								i !== 0 && 'border-t-[0.5px]',
+								j !== 0 && 'border-l-[0.5px]',
+								i !== puzzle.length - 1 && 'border-b-[0.5px]',
+								j !== puzzle.length - 1 && 'border-r-[0.5px]'
+							)}
+							onmouseenter={() => test_trigger(i, j)}
+							onmousedown={(e) => ev_mouse_down(i, j, e)}
+							onmouseup={() => ev_mouse_up()}
+							role="button"
+							tabindex="0"
+							style:background-color={puzzleColor}
+						>
+							{#if cell === 'S'}
+								<div transition:scale class="aspect-square rounded bg-white" style:width="32px"></div>
+							{:else if cell === 'U'}
+								<div transition:scale></div>
+							{:else if cell === 'M'}
+								<div
+										transition:scale
+										class="aspect-square size-8 rounded border-2 border-white"
+								></div>
+							{:else if cell === 'X'}
+								<div transition:scale class="relative aspect-square size-10">
+									<div class="absolute aspect-square size-10">
+										<div class="flex h-full w-full items-center justify-center">
+											<div class="h-8 w-0.5 rotate-45 rounded-full bg-black"></div>
+										</div>
+									</div>
+									<div class="absolute aspect-square size-10">
+										<div class="flex h-full w-full items-center justify-center">
+											<div class="h-8 w-0.5 -rotate-45 rounded-full bg-black"></div>
+										</div>
 									</div>
 								</div>
-								<div class="absolute aspect-square size-10">
-									<div class="flex h-full w-full items-center justify-center">
-										<div class="h-8 w-0.5 -rotate-45 rounded-full bg-black"></div>
-									</div>
-								</div>
-							</div>
-						{/if}
-					</div>
+							{/if}
+						</div>
+					{/each}
 				{/each}
-			{/each}
+			</div>
+			<div id="puzzle-reveal" class="object-fit border-amber-400 ease-linear border-4 bg-cover resize z-10 w-full h-full absolute transition-opacity"
+				 style:image-rendering="pixelated" style:display="none" style:opacity={puzzleFinished ? 1 : 0} style:transition-duration="1500ms"
+			></div>
 		</div>
 	</div>
 </div>
